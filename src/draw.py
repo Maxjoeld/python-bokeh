ab88498fb8d70bbfcd1db453dbf40660c3013b17
Maximo DeLaRosa <mjd809@gmail.com>
2018-06-20 19:00:37 -0400

    Added graph classes

---
+++ src/draw.py
@@ -0,0 +1,45 @@
+import math
+
+from bokeh.io import show, output_file
+from bokeh.plotting import figure
+from bokeh.models import GraphRenderer, StaticLayoutProvider, Oval
+from bokeh.palettes import Spectral8
+
+from graph import *
+
+graph_data = Graph()
+graph_data.debug_create_test_data()
+
+N = 10
+node_indices = list(range(N))
+
+debug_pallete = Spectral8
+debug_pallete.append('#000000')
+debug_pallete.append('#800000')
+
+
+plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 500),
+              tools='', toolbar_location=None)
+
+graph = GraphRenderer()
+
+graph.node_renderer.data_source.add(node_indices, 'index')
+graph.node_renderer.data_source.add(debug_pallete, 'color')
+graph.node_renderer.glyph = Oval(height=0.1, width=0.2, fill_color='color')
+
+graph.edge_renderer.data_source.data = dict(
+    start=[0]*N,
+    end=node_indices)
+
+### start of layout code
+circ = [i*2*math.pi/N for i in node_indices]
+x = [math.cos(i) for i in circ]
+y = [math.sin(i) for i in circ]
+
+graph_layout = dict(zip(node_indices, zip(x, y)))
+graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
+
+plot.renderers.append(graph)
+
+output_file('graph.html')
+show(plot)
