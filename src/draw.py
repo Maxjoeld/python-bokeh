a2185725bf36532b1c5eb55d409586eb5e3d4ec8
Maximo DeLaRosa <mjd809@gmail.com>
2018-06-22 11:13:49 -0400

    Finalized graph

--- src/draw.py
+++ src/draw.py
@@ -11,16 +11,12 @@ graph_data = Graph()
 graph_data.debug_create_test_data()
 print(graph_data.vertexes)
 
-N = 10
+N = len(graph_data.vertexes)
 node_indices = list(range(N))
 
-debug_pallete = Spectral8
-debug_pallete.append('#000000')
-debug_pallete.append('#800000')
-
-# color_list = []
-# for vertex in graph_data.vertexes:
-#   color_list.append(vertex.color)
+color_list = []
+for vertex in graph_data.vertexes:
+    color_list.append(vertex.color)
 
 plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 500),
               tools='', toolbar_location=None)
@@ -28,17 +24,31 @@ plot = figure(title='Graph Layout Demonstration', x_range=(0, 500), y_range=(0, 
 graph = GraphRenderer()
 
 graph.node_renderer.data_source.add(node_indices, 'index')
-graph.node_renderer.data_source.add(debug_pallete, 'color')
+graph.node_renderer.data_source.add(color_list, 'color')
 graph.node_renderer.glyph = Oval(height=10, width=10, fill_color='color')
 
+
+#this is drawing the edges from start to end
+start_indexes = []
+end_indexes = []
+
+for start_index, vertex in enumerate(graph_data.vertexes):
+    for e in vertex.edges:
+        start_indexes.append(start_index)
+        end_indexes.append(graph_data.vertexes.index(e.destination))
+
 graph.edge_renderer.data_source.data = dict(
-    start=[0]*N,
-    end=node_indices)
+    start=start_indexes, # a list of vertex indexes to start edges from
+    end=end_indexes) # a list of vertex indexes to end edges at
+
+print(graph.edge_renderer.data_source.data)
 
 ### start of layout code
-circ = [i*2*math.pi/N for i in node_indices]
-x = [math.cos(i) for i in circ]
-y = [math.sin(i) for i in circ]
+# Looks like this is setting the positions of the vertexes
+
+x = [v.pos['x'] for v in graph_data.vertexes]
+y = [v.pos['y'] for v in graph_data.vertexes]
+
 
 graph_layout = dict(zip(node_indices, zip(x, y)))
 graph.layout_provider = StaticLayoutProvider(graph_layout=graph_layout)
